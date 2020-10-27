411e123314c5f1337e68f6d3fdb246b95a801de3
jungunman <jin02014@naver.com>
2020-10-28 00:01:20 +0900

    포인터와 배열의 관계에 대해서 알아보았다. 의문이 들었다. JAVA에서는 length라는 훌륭한 함수를 제공하여 배열의 길이만큼 편하게 for문을 돌릴 수 있게 해놓았으나, 여기서는 sizeof()라는 함수로 포인터의 총길이에서 하나의 길이를 나눠 돌리라고 한다. 근데 이것은 c언어에서 부합하는 방법일 수도 있다며, range-for문을 사용하라구 한다. range-for문은 익숙하지 않은 바, 익숙할 필요가 생겼다.
    의문 1 : 배열을 만들고 나서 왜 포인터로 대입하여 연산하여야 하는지?
      그 이유와 쓸모에 대해서 알고 싶습니다.
      배열을 만들었으면 그냥 사용하고 끝내면 되지 굳이 포인터에 넣어서 사용해야하나요?

--- ModernCStudy/PointerExam.cpp
+++ ModernCStudy/PointerExam.cpp
@@ -1,19 +1,19 @@
-#include <iostream>
-
-int main()
-{
-
-	int num1 = 100;
-	double num2 = 3.14;
-
-	int* pointer_num1 = &num1;
-	double* pointer_num2 = &num2;
-
-
-	std::cout << "pointer_num1 의 값은 " << sizeof(pointer_num1) << "입니다." << std::endl;
-	std::cout << "pointer_num2 의 값은 " << sizeof(pointer_num2) << "입니다." << std::endl;
-	std::cout << "pointer_num1 의 주소 값은" << pointer_num1 << "이구요 ";
-	std::cout << "pointer_num1 의 주소에 들어있는 값은 " << *pointer_num1 << "입니다." << std::endl;
-	std::cout << "pointer_num2 의 주소 값은" << pointer_num2 << "이구요 ";
-	std::cout << "pointer_num2 의 주소에 들어있는 값은 " << *pointer_num2 << "입니다." << std::endl;
-}
+//#include <iostream>
+//
+//int main()
+//{
+//
+//	int num1 = 100;
+//	double num2 = 3.14;
+//
+//	int* pointer_num1 = &num1;
+//	double* pointer_num2 = &num2;
+//
+//
+//	std::cout << "pointer_num1 의 값은 " << sizeof(pointer_num1) << "입니다." << std::endl;
+//	std::cout << "pointer_num2 의 값은 " << sizeof(pointer_num2) << "입니다." << std::endl;
+//	std::cout << "pointer_num1 의 주소 값은" << pointer_num1 << "이구요 ";
+//	std::cout << "pointer_num1 의 주소에 들어있는 값은 " << *pointer_num1 << "입니다." << std::endl;
+//	std::cout << "pointer_num2 의 주소 값은" << pointer_num2 << "이구요 ";
+//	std::cout << "pointer_num2 의 주소에 들어있는 값은 " << *pointer_num2 << "입니다." << std::endl;
+//}
