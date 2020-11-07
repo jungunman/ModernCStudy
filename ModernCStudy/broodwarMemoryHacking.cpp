b2b2b65bf0742a845fa08b5f2abe4b663b4c47d1
jungunman <jin02014@naver.com>
2020-11-08 02:17:51 +0900

    #스타크래프트 해킹2
    관리자 권한으로 인한 메모리 시스템을 읽어오지 못하는 거였음.
    게임도 비쥬얼스튜디오도 관리자 권한으로 실행시켜 진행해보니 문제없이 메모리의 값을 읽고 변경할 수 있었음.
    
    키 이벤트를 넣어봤으나, 콘솔창에서 입력하지 않으면 작동하지 않기에 주석처리.
    
    반복문이 너무 빨리 돌아 시스템에 과부하가 걸릴까 걱정되어 슬립으로 적당한 속도 조절.
    
    콘솔창에서 입력하지 않고도 윈도우에서 입력하면 키고 끌 수 있게 만드는 방법을 알아내야겠음.

--- ModernCStudy/broodwarMemoryHacking.cpp
+++ ModernCStudy/broodwarMemoryHacking.cpp
@@ -13,7 +13,7 @@ int main() {
 	DWORD motion = 0x0061c3d8; //미네랄 캤는가 유무 주소
 
 	//게임 실행 및 키 이벤트
-	bool isRunning = FALSE; //게임 실행 중?
+	bool isRunning = TRUE; //게임 실행 중?
 	char key_event = NULL; // 무슨 키 눌림?
 
 	//메모리 사이즈 보기
@@ -33,18 +33,23 @@ int main() {
 	std::cout << "Pid : " << pid << std::endl;
 	pHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
 
-	key_event = _getch();
-	//핵 키고 끄기
-	if (key_event == '0') {
-		std::cout << "KEY_EVENT : " << key_event << std::endl;
-		isRunning = TRUE;
-	}
-	else if (key_event == '9') {
-		isRunning = FALSE;
-		std::cout << "KEY_EVENT : " << key_event << std::endl;
-	}
+
+
+
 
 	while (isRunning) {
+
+		//ReadProcessMemory(pHandle, (DWORD*)address[i], &value, 4, &readSize); //메모리 잘 접근했는지 디버깅용
+		//(pHandle, (DWORD*)address[i], &c_value, sizeof(c_value), &readSize); // 미네랄 및 가스 증가용
+		WriteProcessMemory(pHandle, (DWORD*)catch_mineral_address, &catch_value, sizeof(catch_value), 0); // 미네랄 캘 시 증가량 표시하는 주소
+		WriteProcessMemory(pHandle, (DWORD*)motion, &motion_value, sizeof(motion_value), 0); //미네랄 캤는지 아닌지 유무 판단하는 주소
+		printf("%x , %d \n", catch_mineral_address, catch_value);//디버깅용
+		printf("%x , %d \n", motion, motion_value);//디버깅용
+
+		Sleep(2000);//너무 빠른 반복은 불필요하기에 2초마다 작동하도록 만듬.
+
+		//키 이벤트 삽입
+		/*key_event = _getch();
 		if (key_event == '0') {
 			std::cout << "KEY_EVENT : " << key_event << std::endl;
 			isRunning = TRUE;
@@ -52,13 +57,8 @@ int main() {
 		else if (key_event == '9') {
 			isRunning = FALSE;
 			std::cout << "KEY_EVENT : " << key_event << std::endl;
-		}
-		//ReadProcessMemory(pHandle, (DWORD*)address[i], &value, 4, &readSize); //메모리 잘 접근했는지 디버깅용
-		//(pHandle, (DWORD*)address[i], &c_value, sizeof(c_value), &readSize); // 미네랄 및 가스 증가용
-		WriteProcessMemory(pHandle, (DWORD*)catch_mineral_address, &catch_value, sizeof(catch_value), 0); // 미네랄 캘 시 증가량 표시하는 주소
-		WriteProcessMemory(pHandle, (DWORD*)motion, &motion_value, sizeof(motion_value), 0); //미네랄 캤는지 아닌지 유무 판단하는 주소
-		printf("%x , %d \n", catch_mineral_address, catch_value);//디버깅용
-		printf("%x , %d \n", motion, motion_value);//디버깅용
+		}*/
+
 	}
 
 	
